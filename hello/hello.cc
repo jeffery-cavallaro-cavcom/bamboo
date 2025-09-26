1f77cc617e469a504ae33e032b6b4391e4b7e4aa
Jeffery Cavallaro <jeffery@cavcom.com>
2025-09-26 02:43:33 -0700

    Where to find name

--- hello/hello.cc
+++ hello/hello.cc
@@ -2,10 +2,27 @@
  * A simple hello world program.
  */
 
+ #include <cctype>
+ #include <cstdlib>
+
+ #include <algorithm>
  #include <iostream>
+ #include <string>
 
  int main(int argc, char *argv[]) {
-    std::cout << "Hello, World !!!" << std::endl;
+    std::string who("world");
+    if (argc > 1) {
+        who = argv[1];
+    } else {
+        char *value = std::getenv("USER");
+        if (value) {
+            who = value;
+        }
+    }
+
+    std::transform(who.begin(), who.end(), who.begin(), ::tolower);
+    if (who.length() > 0) who[0] = std::toupper(who[0]);
+    std::cout << "Hello, " << who << " !!!" << std::endl;
+
     return 0;
  }
- 
